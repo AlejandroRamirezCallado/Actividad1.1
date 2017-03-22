dd6fc91fa9e82f7a4a52789002fe15b9336aaf49
Alejandro ramirez <alejandro.ramirez@macalmpu0133.local>
2017-03-22 18:03:21 +0100

    Commit

--- Actividad1.1/ViewController.swift
+++ Actividad1.1/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var btnLogear:UIButton?
+    @IBOutlet var txtfUsuario:UITextField?
+    @IBOutlet var txtfContraseña:UITextField?
+    @IBOutlet var txtVConsola:UITextView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func accionBotonLogear() {
+        //txtVConsola?.text="hola mundo"
+        txtVConsola?.text=String(format: "HOLA MUNDO USUARIO: %@ PASSWORD: %@", (txtfUsuario?.text)!,(txtfContraseña?.text)!)
+        
     }
 
 
